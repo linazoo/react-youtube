c6e2b7ce017faf708adf86eb3a7da73632c2a4a2
Lina Zoo <linazooper@gmail.com>
2017-11-21 21:25:13 -0600

    search capability works - linking video links in playlist working, add basic styling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -17,12 +17,17 @@ class SearchBar extends Component {
     //defining methods on a class:
     render() {
         return (
-            <div>
+            <div className="search-bar">
                 <input 
-                value={this.state.term}
-                onChange={event => this.setState({ term: event.target.value })} />
+                value={ this.state.term }
+                onChange={ event => this.onInputChange(event.target.value) } />
             </div>
         );
+    }
+
+    onInputChange(term) {
+        this.setState({ term });
+        this.props.onSearchTermChange(term);
     }
 }
 
